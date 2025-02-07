22f09e986bbaecb43b0c3d55512dc78e1c57d90f
Shreya Kore <shreyakore07@gmail.com>
2025-02-07 18:01:38 +0530

    first commit

---
+++ Desktop/New folder/loops.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class loops {
+
+    public static void main(String args []) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+         for(int i = 2; i <=11 ; i++ ) {
+              System.out.println(n+i);
+    }
+    
+
+
+
+
+    
+       
+    }
+}
+    
+
+
+
